6f942d4af994ff70a7597b19329c9f41129853ca
sagyak <110639277+sagyak@users.noreply.github.com>
2022-09-12 13:53:32 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,5 +33,5 @@ resource "snowflake_database" "myclone" {
   name                        = "CLONE_CITIBIKE"
   comment                     = "test comment"
   data_retention_time_in_days = 3
-  from_replica                = "org1\".\"account1\"."\CITIBIKE"
+  from_replica                = "SNOWFLAKE\".\"ku73901\".\"CITIBIKE"
 }
