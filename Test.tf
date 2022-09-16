96d0a8d7826f6c52cf2900cf34c94ba76143aa22
sagyak <110639277+sagyak@users.noreply.github.com>
2022-09-16 11:29:15 +1000

    Update Test.tf

--- Test.tf
+++ Test.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
-    }
-  }
-
-  backend "remote" {
-    organization = "sagya"
-
-    workspaces {
-      name = "CICD"
-    }
-  }
-}
-
-provider "snowflake" {
-}
 
 resource "snowflake_database" "test_db" {
   name    = "TEST_DB"
